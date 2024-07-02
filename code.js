e93b9b0febde388df70fcc9ad770a3d2835bacbc
Gustavo Mello <gustavomello9178@gmail.com>
2024-07-02 19:44:23 -0300

    fix(code-page): Alteração na condição dos ifs, para não ter mais de uma saida simultanea

--- code.js
+++ code.js
@@ -4,10 +4,10 @@ num3 = prompt("Terceiro numero")
 num4 = prompt("Quarto numero")
 num5 = prompt("Quinto numero")
 
-if (num1 % 3 == 0){
+if (num1 % 3 == 0 && num1 % 5 != 0){
     console.log("fizz")
 }
-if (num1 % 5 == 0){
+if (num1 % 5 == 0 && num1 % 3 != 0){
     console.log("buzz")
 }
 if (num1 % 3 == 0 && num1 % 5 == 0){
@@ -15,10 +15,10 @@ if (num1 % 3 == 0 && num1 % 5 == 0){
 }
 
 
-if (num2 % 3 == 0){
+if (num2 % 3 == 0 && num2 % 5 != 0){
     console.log("fizz")
 }
-if (num2 % 5 == 0){
+if (num2 % 5 == 0 && num2 % 3 != 0){
     console.log("buzz")
 }
 if (num2 % 3 == 0 && num2 % 5 == 0){
@@ -26,10 +26,10 @@ if (num2 % 3 == 0 && num2 % 5 == 0){
 }
 
 
-if (num3 % 3 == 0){
+if (num3 % 3 == 0 && num3 % 5 != 0){
     console.log("fizz")
 }
-if (num3 % 5 == 0){
+if (num3 % 5 == 0 && num3 % 3 != 0){
     console.log("buzz")
 }
 if (num3 % 3 == 0 && num3 % 5 == 0){
@@ -37,20 +37,20 @@ if (num3 % 3 == 0 && num3 % 5 == 0){
 }
 
 
-if (num4 % 3 == 0){
+if (num4 % 3 == 0 && num4 % 5 != 0){
     console.log("fizz")
 }
-if (num4 % 5 == 0){
+if (num4 % 5 == 0 && num4 % 3 != 0){
     console.log("buzz")
 }
 if (num4 % 3 == 0 && num4 % 5 == 0){
     console.log("fizzbuzz")
 }
 
-if (num5 % 3 == 0){
+if (num5 % 3 == 0 && num5 % 5 != 0){
     console.log("fizz")
 }
-if (num5 % 5 == 0){
+if (num5 % 5 == 0 && num5 % 3 != 0){
     console.log("buzz")
 }
 if (num5 % 3 == 0 && num5 % 5 == 0){
