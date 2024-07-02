20d07b5b2b18a4c4e946fa1a86b60e6c9ee2ae6e
Gustavo Mello <gustavomello9178@gmail.com>
2024-07-02 19:28:43 -0300

    feat(code-page): Conjunto de testes da primeira variavel

--- code.js
+++ code.js
@@ -3,3 +3,13 @@ num2 = prompt("Segundo numero")
 num3 = prompt("Terceiro numero")
 num4 = prompt("Quarto numero")
 num5 = prompt("Quinto numero")
+
+if (num1 % 3 == 0){
+    console.log("fizz")
+}
+if (num1 % 5 == 0){
+    console.log("buzz")
+}
+if (num1 % 3 == 0 && num1 % 5 == 0){
+    console.log("fizzbuzz")
+}
